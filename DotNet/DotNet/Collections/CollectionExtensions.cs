85253cf7d9214a2528f64ef671a692412db38b49
Viktor Faddi <faddiv@gmail.com>
2020-04-01 19:04:44 +0200

    Implement AddRange and AddElements on ICollection.

---
+++ DotNet/DotNet/Collections/CollectionExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Faddiv.DotNet.Collections
+{
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        ///     Adds the elements of the specified collection to the end of 
+        ///     the <see cref="ICollection{TElement}"/>. The collection has to 
+        ///     support <see cref="ICollection{TElement}.Add(TElement)">Add(TElement)</see>.
+        /// </summary>
+        /// <typeparam name="TCollection">The type of the list.</typeparam>
+        /// <typeparam name="TElement">The type of elements in the list.</typeparam>
+        /// <param name="collection">
+        ///     The collection where the elements should be added. It has to support <see cref="ICollection{T}.Add"/>.
+        /// </param>
+        /// <param name="elements">
+        ///     The collection whose elements should be added to the end of 
+        ///     the <see cref="ICollection{TElement}"/>. The collection itself cannot 
+        ///     be null, but it can contain elements that are null, if type TElement
+        ///     is a reference type.
+        /// </param>
+        public static TCollection AddRange<TCollection, TElement>(this TCollection collection, IEnumerable<TElement> elements)
+            where TCollection : class, ICollection<TElement>
+        {
+            if (collection is null)
+                throw new ArgumentNullException(nameof(collection));
+            if (elements is null)
+                throw new ArgumentNullException(nameof(elements));
+
+            foreach (var item in elements)
+            {
+                collection.Add(item);
+            }
+            return collection;
+        }
+
+        /// <summary>
+        ///     Adds the elements of the specified collection to the end of 
+        ///     the <see cref="ICollection{TElement}"/>. The collection has to 
+        ///     support <see cref="ICollection{TElement}.Add(TElement)">Add(TElement)</see>.
+        /// </summary>
+        /// <typeparam name="TCollection">The type of the list.</typeparam>
+        /// <typeparam name="TElement">The type of elements in the list.</typeparam>
+        /// <param name="collection">
+        ///     The collection where the elements should be added. It has to support <see cref="ICollection{T}.Add"/>.
+        /// </param>
+        /// <param name="elements">
+        ///     The invidual elements that should be added to the end of 
+        ///     the <see cref="ICollection{TElement}"/>. The collection itself cannot 
+        ///     be null, but it can contain elements that are null, if type TElement
+        ///     is a reference type.
+        /// </param>
+        public static TCollection AddElements<TCollection, TElement>(this TCollection collection, params TElement[] elements)
+            where TCollection : class, ICollection<TElement>
+        {
+            return collection.AddRange(elements.AsEnumerable());
+        }
+    }
+}
---
+++ DotNet/Faddiv.DotNet.Tests/Collections/CollectionExtensionsTests.cs
@@ -0,0 +1,120 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Faddiv.DotNet.Collections
+{
+    public class CollectionExtensionsTests
+    {
+        [Fact]
+        public void AddRange_adds_multiple_elements()
+        {
+            // Arrange
+            ICollection<int> list = Enumerable.Range(0, 3).ToList();
+            var newElements = Enumerable.Range(3, 3);
+            var expected = Enumerable.Range(0, 6).ToList();
+
+            // Act
+            list.AddRange(newElements);
+
+            // Assert
+            list.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void AddRange_requires_the_list()
+        {
+            // Arrange
+            ICollection<int> list = null;
+            var newElements = Enumerable.Range(3, 3);
+            Action action = () => list.AddRange(newElements);
+
+            // Act
+            // Assert
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void AddRange_requires_the_collection()
+        {
+            // Arrange
+            ICollection<int> list = Enumerable.Range(3, 3).ToList();
+            IEnumerable<int> newElements = null;
+            Action action = () => list.AddRange(newElements);
+
+            // Act
+            // Assert
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void AddRange_returns_the_collection()
+        {
+            // Arrange
+            ICollection<int> list = Enumerable.Range(0, 3).ToList();
+            var newElements = Enumerable.Range(3, 3);
+
+            // Act
+            var result = list.AddRange(newElements);
+
+            // Assert
+            result.Should().BeSameAs(list);
+        }
+
+        [Fact]
+        public void AddElements_adds_multiple_elements()
+        {
+            // Arrange
+            ICollection<int> list = Enumerable.Range(0, 3).ToList();
+            var expected = Enumerable.Range(0, 6).ToList();
+
+            // Act
+            list.AddElements(3, 4, 5);
+
+            // Assert
+            list.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void AddElements_requires_the_list()
+        {
+            // Arrange
+            ICollection<int> list = null;
+            var newElements = Enumerable.Range(3, 3).ToArray();
+            Action action = () => list.AddElements(newElements);
+
+            // Act
+            // Assert
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void AddElements_requires_the_collection()
+        {
+            // Arrange
+            ICollection<int> list = Enumerable.Range(3, 3).ToList();
+            int[] newElements = null;
+            Action action = () => list.AddElements(newElements);
+
+            // Act
+            // Assert
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void AddElements_returns_the_collection()
+        {
+            // Arrange
+            ICollection<int> list = Enumerable.Range(0, 3).ToList();
+
+            // Act
+            var result = list.AddElements(1,2);
+
+            // Assert
+            result.Should().BeSameAs(list);
+        }
+    }
+}
