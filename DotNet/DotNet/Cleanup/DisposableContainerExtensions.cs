ab2a443b8a7dbb8b31d3a77fc18c3d9053275933
Viktor Faddi <faddiv@gmail.com>
2020-04-16 21:37:07 +0200

    Implement cleanup helpers.

---
+++ DotNet/DotNet/Cleanup/DisposableContainerExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Faddiv.DotNet.Cleanup
+{
+    /// <summary>
+    /// Connects other classes from this namespace to the DisposableContainer.
+    /// </summary>
+    public static partial class DisposableContainerExtensions
+    {
+        public static void AddCleanupAction(this DisposableContainer container, Action cleanupAction)
+        {
+            container.AddManagedResource(new CleanupAction(cleanupAction));
+        }
+
+        /// <summary>
+        /// Subscribe the handler for the given event and adds the unsubscription 
+        /// to the DisposableContanier so it is called on Dispose.
+        /// </summary>
+        /// <param name="container">The container where the event unsubscription is added.</param>
+        /// <param name="instance">The instance which contains the event.</param>
+        /// <param name="eventName">The name of the event. It is case sensitive.</param>
+        /// <param name="handler">The handler which are subscripted to the event.</param>
+        public static void AddEventSubscription(this DisposableContainer container, 
+            object instance,
+            string eventName,
+            Delegate handler)
+        {
+            container.AddManagedResource(new EventSubscription(instance, eventName, handler));
+        }
+    }
+}
---
+++ DotNet/Faddiv.DotNet.Tests/Cleanup/DisposabeContainerExtensionsTests.cs
@@ -0,0 +1,56 @@
+﻿using Moq;
+using System;
+using Xunit;
+
+namespace Faddiv.DotNet.Cleanup
+{
+    public class DisposabeContainerExtensionsTests
+    {
+
+        [Fact]
+        public void AddCleanupAction_adds_CleanupAction_to_the_DisposableContainer()
+        {
+            var container = new DisposableContainer();
+            var mock1 = new Mock<Action>();
+
+            container.AddCleanupAction(mock1.Object);
+            container.Dispose();
+
+            mock1.Verify(d => d(), Times.Once);
+        }
+
+        [Fact]
+        public void AddEventSubscription_adds_delegate_to_the_DisposableContainer()
+        {
+
+            var container = new DisposableContainer();
+            var instanceMock = new Mock<IDummyObserver>();
+            var source = new DummyEventSource();
+            var args = new EventArgs();
+
+            container.AddEventSubscription(source, nameof(source.InstanceHandler1), new EventHandler(instanceMock.Object.EventHandler));
+            source.InvokeInstanceHandler1(args);
+            container.Dispose();
+            source.InvokeInstanceHandler1(args);
+
+            instanceMock.Verify(d => d.EventHandler(It.IsAny<object>(), It.IsAny<EventArgs>()), Times.Once);
+        }
+
+        [Fact]
+        public void AddEventSubscription_adds_Action_to_the_DisposableContainer()
+        {
+
+            var container = new DisposableContainer();
+            var instanceMock = new Mock<IDummyObserver>();
+            var source = new DummyEventSource();
+            var args = new EventArgs();
+
+            container.AddEventSubscription<object, EventArgs>(source, nameof(source.InstanceHandler1), instanceMock.Object.EventHandler);
+            source.InvokeInstanceHandler1(args);
+            container.Dispose();
+            source.InvokeInstanceHandler1(args);
+
+            instanceMock.Verify(d => d.EventHandler(It.IsAny<object>(), It.IsAny<EventArgs>()), Times.Once);
+        }
+    }
+}
