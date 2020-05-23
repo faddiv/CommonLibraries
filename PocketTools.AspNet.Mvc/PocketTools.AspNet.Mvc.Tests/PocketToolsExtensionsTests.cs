3761c33025f2a2bd50211c5d0e36e69fa2f5a381
Viktor Faddi <faddiv@gmail.com>
2020-05-23 16:17:14 +0200

    Add PocketTools.AspNet.Mvc with Html.GetUrlHelper() extension

---
+++ PocketTools.AspNet.Mvc/PocketTools.AspNet.Mvc.Tests/PocketToolsExtensionsTests.cs
@@ -0,0 +1,95 @@
+﻿using FluentAssertions;
+using Moq;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Xunit;
+
+namespace PocketTools.AspNet.Mvc
+{
+    public class PocketToolsExtensionsTests
+    {
+        [Fact]
+        public void GetUrlHelper_returns_UrlHelper_from_WebViewPage()
+        {
+            var mock = new Mock<WebViewPage>()
+            {
+                CallBase = true
+            };
+            var page = mock.Object;
+            page.ViewContext = new ViewContext();
+            page.ViewData = new ViewDataDictionary();
+            page.InitHelpers();
+
+            var url = page.Html.GetUrlHelper();
+
+            url.Should().BeSameAs(page.Url);
+        }
+
+        [Fact]
+        public void GetUrlHelper_returns_UrlHelper_from_ViewPage()
+        {
+            var page = new ViewPage
+            {
+                ViewContext = new ViewContext(),
+                ViewData = new ViewDataDictionary()
+            };
+            page.InitHelpers();
+
+            var url = page.Html.GetUrlHelper();
+
+            url.Should().BeSameAs(page.Url);
+        }
+
+        [Fact]
+        public void GetUrlHelper_returns_UrlHelper_from_ViewMasterPage()
+        {
+            var page = new ViewPage
+            {
+                ViewContext = new ViewContext(),
+                ViewData = new ViewDataDictionary()
+            };
+            page.InitHelpers();
+            var masterPage = new ViewMasterPage
+            {
+                Page = page
+            };
+
+            var url = masterPage.Html.GetUrlHelper();
+
+            url.Should().BeSameAs(page.Url);
+        }
+
+        [Fact]
+        public void GetUrlHelper_returns_UrlHelper_from_ViewUserControl()
+        {
+            var control = new ViewUserControl();
+            ViewPage viewPage = new ViewPage();
+            viewPage.ViewContext = new ViewContext();
+            viewPage.ViewData = new ViewDataDictionary();
+            viewPage.InitHelpers();
+            viewPage.Controls.Add(control);
+
+            var url = control.Html.GetUrlHelper();
+
+            url.Should().BeSameAs(control.Url);
+        }
+
+        [Fact]
+        public void GetUrlHelper_returns_new_UrlHelper_on_diferent_type()
+        {
+            var viewDataContainer = new Mock<IViewDataContainer>();
+            var viewContext = new Mock<ViewContext>();
+            var routeCollection = new RouteCollection();
+            var helper = new HtmlHelper(
+                viewContext.Object, 
+                viewDataContainer.Object,
+                routeCollection);
+
+            var url = helper.GetUrlHelper();
+
+            url.Should().NotBeNull();
+            url.RouteCollection.Should().BeSameAs(routeCollection);
+            url.RequestContext.Should().BeSameAs(viewContext.Object.RequestContext);
+        }
+    }
+}
---
+++ PocketTools.AspNet.Mvc/PocketTools.AspNet.Mvc/PocketToolsExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace PocketTools.AspNet.Mvc
+{
+    public static class PocketToolsExtensions
+    {
+        /// <summary>
+        /// Returns the UrlHelper associated with the current context on cshtml, aspx, ascx 
+        /// or creates a new if not found.
+        /// </summary>
+        /// <param name="htmlHelper">Html property on the WebViewPage, ViewPage, ViewMasterPage or ViewUserControl.</param>
+        /// <returns>Same instance of the page or a new instance if not found.</returns>
+        /// <exception cref="ArgumentNullException">The htmlHelper is null.</exception>
+        public static UrlHelper GetUrlHelper(this HtmlHelper htmlHelper)
+        {
+            if (htmlHelper is null)
+                throw new ArgumentNullException(nameof(htmlHelper));
+
+            UrlHelper urlHelper;
+            switch (htmlHelper.ViewDataContainer)
+            {
+                case WebViewPage wvp:
+                    urlHelper = wvp.Url;
+                    break;
+                case ViewPage vp:
+                    urlHelper = vp.Url;
+                    break;
+                case ViewUserControl vuc:
+                    urlHelper = vuc.Url;
+                    break;
+                case ViewMasterPage vmp:
+                    urlHelper = vmp.Url;
+                    break;
+                default:
+                    urlHelper = null;
+                    break;
+            }
+            return urlHelper
+                ?? new UrlHelper(htmlHelper.ViewContext.RequestContext, htmlHelper.RouteCollection);
+            
+        }
+    }
+}
